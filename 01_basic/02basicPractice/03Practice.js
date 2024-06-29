f8cff52c21891d7c4827a00c7b8f9d958cecb1e2
prashantbhoite123 <pbhoite985@gmail.com>
2024-06-29 16:14:08 +0530

    revision

---
+++ 01_basic/02basicPractice/03Practice.js
@@ -0,0 +1,18 @@
+const arr = [1, 2, 3, 4, 5, 6, 7, 8]
+
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i])
+// }
+
+const newarr = [11, 1, 2, 3, 7, 23, 34, , 45, 56, 70, 23, 90]
+
+for (let i = 0; i < newarr.length; i++) {
+  if (newarr[i] % 2 === 0) {
+    console.log(newarr[i])
+  }
+}
+
+// for (let i = arr.length; i >= 0; i--) {
+//   console.log(arr[i])
+
+// }
--- 06_itrator/practice.c
+++ 06_itrator/practice.c
@@ -1,8 +1,18 @@
+// #include<stdio.h>
+// int main(){
+//     int arr = [1,2,3,4,5,6,7,8]
+//     for(int i=0;i<8;i++){
+//         int sum = sum + arr;
+//     }
+//     printf("%d",sum);
+// 
+
+
+
 #include<stdio.h>
 int main(){
-    int arr = [1,2,3,4,5,6,7,8]
-    for(int i=0;i<8;i++){
-        int sum = sum + arr;
-    }
-    printf("%d",sum);
+    int a =20;
+    int b = 20
+    printf(a+b)
+    return 0
 }
